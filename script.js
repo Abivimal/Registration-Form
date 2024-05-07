d255e91ff518e88d6766094920e05765168d6961
ABISHEK VIMAL M <117515435+Abivimal@users.noreply.github.com>
2024-05-07 11:19:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+let form=document.querySelector("#form")
+let username=document.querySelector("#username")
+let email=document.querySelector("#email")
+let password=document.querySelector("#password")
+let cpassword=document.querySelector("#cpassword")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
